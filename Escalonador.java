6a4a0c183f8f52daa4880cd013daa5e6ffb3395b
Amauri de Melo Junior <amaurimelojunior@gmail.com>
2016-10-09 22:24:50 -0300

    Correcoes de bugs e compilacao

--- Escalonador.java
+++ Escalonador.java
@@ -1,106 +1,75 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Scanner;
-public class Escalonador
-{
-  protected int quantum;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
-  public static void main(String [] args)
-  {
-    //codigo aqui
-    Scanner ler = new Scanner(System.in);
-    String [] arquivos;
-    arquivos = new String[10];
-    arquivos[0] = "01.txt";
-    arquivos[1] = "02.txt";
-    arquivos[2] = "03.txt";
-    arquivos[3] = "04.txt";
-    arquivos[4] = "05.txt";
-    arquivos[5] = "06.txt";
-    arquivos[6] = "07.txt";
-    arquivos[7] = "08.txt";
-    arquivos[8] = "09.txt";
-    arquivos[9] = "10.txt";
-    // arquivos[10] = "quantum.txt";
+public class Escalonador {
+    protected int quantum;
+    protected int total;
+    LinkedList<Processo> prontos;
+    LinkedList<Processo> bloqueados;
 
-    LinkedList<Processo> prontos = new LinkedList<Processo>();
-    LinkedList<Processo> bloqueados = new LinkedList<Processo>();
+    public Escalonador(int quantum) {
+        this.quantum = quantum;
 
-    try{
-      FileReader arqQuantum = new FileReader("quantum.txt");
-      BufferedReader lerQuantum = new BufferedReader(arqQuantum);
-      this.quantum = Integer.parseInt(lerQuantum.readLine());
-      arqQuantum.close();
-      System.out.printf("Quantum: " + this.quantum);  
+        this.total = 0;
+        this.prontos = new LinkedList<Processo>();
+        this.bloqueados = new LinkedList<Processo>();
+    }
 
-      String nome;
-      
-      System.out.printf("\nConteúdo do arquivo texto:\n");
+    public void criaProcesso(String nome, String [] instrucoes) {
+        Processo novo = new Processo(nome, instrucoes);
+        if (novo.getEstado() == 0) {
+            this.prontos.push(novo);
+        }
+    }
 
-      for(int i = 0; i < 10; i++){
-        nome = arquivos[i];
-        System.out.println("Nome: " + nome);
-        String [] instrucoes = new String[21];
-        int j = 0;
-        try {
-          FileReader arq = new FileReader(nome);
-          BufferedReader lerArq = new BufferedReader(arq);
-          String nome = lerArq.readLine(); // lê a primeira linha
+    public int getQuantum() {
+        return this.quantum;
+    }
 
-          String line = lerArq.readLine();
-          while (line != null) {
-            instrucoes[i] = new String(line);
-            j++;
-            line = lerArq.readLine();
-          }
+    public void executa() {
+        System.out.println("Comecou");
+        while(this.prontos.size() > 0 || this.bloqueados.size() > 0) {
+            this.total++;
+            Processo atual = null;
+            if (this.prontos.size() > 0) {
+                System.out.println("Ha " + this.prontos.size() + " processos com estado 'pronto'");
+                int roundRobin = (int)(Math.random() * this.prontos.size());
+                atual = this.prontos.remove(roundRobin);
+                atual.executa(this.quantum);
 
-          Processo novo = new Processo(instrucoes);
-          if (novo.getEstado() == 0) {
-            prontos.push(novo);
-          }
+                System.out.println("Executando processo " + roundRobin + " - " + atual.getNome() + " - " + atual.getEstado());
+            }
 
-          arq.close();
-        } catch (IOException e) {
-            System.err.printf("Erro na abertura do arquivo: %s.\n",
-              e.getMessage());
+            if (this.bloqueados.size() > 0) {
+                System.out.println("Ha " + this.bloqueados.size() + " com estado 'bloqueado'");
+                ListIterator<Processo> bloqueadosIterator = this.bloqueados.listIterator(0);
+                while (bloqueadosIterator.hasNext()) {
+                    int indiceAtual = bloqueadosIterator.nextIndex();
+                    Processo bloqueadoAtual = bloqueadosIterator.next();
+
+                    bloqueadoAtual.espera();
+
+                    if (bloqueadoAtual.getEstado() == 0) {
+                        System.out.println("Processo " + bloqueadoAtual.getNome() + " está pronto agora");
+                        this.prontos.push(this.bloqueados.remove(indiceAtual));
+                    }
+                }
+            }
+
+            if (atual != null) {
+                switch (atual.getEstado()) {
+                    case 0:
+                        this.prontos.push(atual);
+                        break;
+                    case 1:
+                        this.bloqueados.push(atual);
+                        break;
+                }
+            }
         }
 
-        System.out.println();
-      }
-    }
-    catch (IOException e) {
-          System.err.printf("Erro na abertura do arquivo: %s.\n",
-            e.getMessage());
+        System.out.println("Terminou");
+        System.out.println("Foram necessarios " + total + " quanta para executar os processos");
     }
 
-    while(prontos.size() > 0 || bloqueados.size() > 0) {
-      int roundRobin = (int)(Math.random() * prontos.size());
-
-      Processo atual = prontos.remove(roundRobin);
-      atual.executa();
-
-      if (bloqueados.size() > 0) {
-        ListIterator<Processo> bloqueadosIterator = bloqueados.ListIterator(0);
-        while (bloqueadosIterator.hasNext()) {
-          Processo bloqueadoAtual = bloqueadosIterator.next();
-          bloqueadoAtual.espera();
-        }
-      }
-
-      switch (atual.getEstado()) {
-        case 0:
-          prontos.add(atual);
-          break;
-        case 1:
-          bloqueados.add(atual);
-          break;
-        case 2:
-          // corre pro abraco
-      }
-
-
-
-    }
-  }
 }
